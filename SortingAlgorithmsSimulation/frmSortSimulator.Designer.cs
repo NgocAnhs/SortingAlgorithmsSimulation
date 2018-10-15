afcfc052c209121cce09f3f47f2d426d2d9d3822
NgocAnhs <ngocanhgidi@gmail.com>
2018-10-15 16:49:05 +0700

    interchange, insertion, bubble, selection, quick, heap(move)

---
+++ SortingAlgorithmsSimulation/frmSortSimulator.Designer.cs
@@ -0,0 +1,221 @@
+﻿namespace SortingAlgorithmsSimulation
+{
+    partial class frmSortSimulator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnCreateArr = new System.Windows.Forms.Button();
+            this.txtNumArr = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.cboSortAlgo = new System.Windows.Forms.ComboBox();
+            this.btnSort = new System.Windows.Forms.Button();
+            this.pnlSimulator = new System.Windows.Forms.Panel();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.bgwSimuSort = new System.ComponentModel.BackgroundWorker();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.btnPause = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnCreateArr);
+            this.groupBox1.Controls.Add(this.txtNumArr);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Font = new System.Drawing.Font("Segoe UI Semibold", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(373, 186);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Tạo Mảng";
+            // 
+            // btnCreateArr
+            // 
+            this.btnCreateArr.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.btnCreateArr.Location = new System.Drawing.Point(153, 121);
+            this.btnCreateArr.Name = "btnCreateArr";
+            this.btnCreateArr.Size = new System.Drawing.Size(143, 42);
+            this.btnCreateArr.TabIndex = 2;
+            this.btnCreateArr.Text = "Khởi tạo";
+            this.btnCreateArr.UseVisualStyleBackColor = true;
+            this.btnCreateArr.Click += new System.EventHandler(this.btnCreateArr_Click);
+            // 
+            // txtNumArr
+            // 
+            this.txtNumArr.Location = new System.Drawing.Point(133, 57);
+            this.txtNumArr.Name = "txtNumArr";
+            this.txtNumArr.Size = new System.Drawing.Size(206, 38);
+            this.txtNumArr.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(28, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(99, 62);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Số phần tử mảng:";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.cboSortAlgo);
+            this.groupBox2.Controls.Add(this.btnSort);
+            this.groupBox2.Font = new System.Drawing.Font("Segoe UI Semibold", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox2.Location = new System.Drawing.Point(402, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(373, 186);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Thuật toán";
+            // 
+            // cboSortAlgo
+            // 
+            this.cboSortAlgo.FormattingEnabled = true;
+            this.cboSortAlgo.Items.AddRange(new object[] {
+            "Interchange Sort",
+            "Insertion Sort",
+            "Selection Sort",
+            "Bubble Sort",
+            "Heap Sort",
+            "Quick Sort (first pivot)",
+            "Merge Sort",
+            "Shell Sort"});
+            this.cboSortAlgo.Location = new System.Drawing.Point(53, 57);
+            this.cboSortAlgo.Name = "cboSortAlgo";
+            this.cboSortAlgo.Size = new System.Drawing.Size(284, 39);
+            this.cboSortAlgo.TabIndex = 7;
+            // 
+            // btnSort
+            // 
+            this.btnSort.Enabled = false;
+            this.btnSort.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.btnSort.Location = new System.Drawing.Point(108, 121);
+            this.btnSort.Name = "btnSort";
+            this.btnSort.Size = new System.Drawing.Size(143, 42);
+            this.btnSort.TabIndex = 6;
+            this.btnSort.Text = "Sắp xếp";
+            this.btnSort.UseVisualStyleBackColor = true;
+            this.btnSort.Click += new System.EventHandler(this.btnSort_Click);
+            // 
+            // pnlSimulator
+            // 
+            this.pnlSimulator.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pnlSimulator.AutoScroll = true;
+            this.pnlSimulator.BackColor = System.Drawing.Color.White;
+            this.pnlSimulator.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pnlSimulator.Location = new System.Drawing.Point(12, 216);
+            this.pnlSimulator.Name = "pnlSimulator";
+            this.pnlSimulator.Size = new System.Drawing.Size(982, 321);
+            this.pnlSimulator.TabIndex = 4;
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(10, 57);
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(165, 56);
+            this.trackBar1.TabIndex = 5;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // bgwSimuSort
+            // 
+            this.bgwSimuSort.WorkerReportsProgress = true;
+            this.bgwSimuSort.DoWork += new System.ComponentModel.DoWorkEventHandler(this.bgwSimuSort_DoWork);
+            this.bgwSimuSort.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.bgwSimuSort_ProgressChanged);
+            this.bgwSimuSort.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.bgwSimuSort_RunWorkerCompleted);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.btnPause);
+            this.groupBox3.Controls.Add(this.trackBar1);
+            this.groupBox3.Font = new System.Drawing.Font("Segoe UI Semibold", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox3.Location = new System.Drawing.Point(792, 12);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(201, 186);
+            this.groupBox3.TabIndex = 6;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Điều Khiển";
+            // 
+            // btnPause
+            // 
+            this.btnPause.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.btnPause.Location = new System.Drawing.Point(43, 121);
+            this.btnPause.Name = "btnPause";
+            this.btnPause.Size = new System.Drawing.Size(98, 42);
+            this.btnPause.TabIndex = 6;
+            this.btnPause.Text = "Pause";
+            this.btnPause.UseVisualStyleBackColor = true;
+            // 
+            // frmSortSimulator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 28F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1006, 721);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.pnlSimulator);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "frmSortSimulator";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Sorting Algorithms Simulation";
+            this.Load += new System.EventHandler(this.frmSortSimulator_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Panel pnlSimulator;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnCreateArr;
+        private System.Windows.Forms.TextBox txtNumArr;
+        private System.Windows.Forms.Button btnSort;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.ComboBox cboSortAlgo;
+        private System.ComponentModel.BackgroundWorker bgwSimuSort;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button btnPause;
+    }
+}
+
---
+++ SortingAlgorithmsSimulation/frmSortSimulator.cs
@@ -0,0 +1,484 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SortingAlgorithmsSimulation
+{
+    public partial class frmSortSimulator : Form
+    {
+        public frmSortSimulator()
+        {
+            InitializeComponent();
+        }
+
+        private void frmSortSimulator_Load(object sender, EventArgs e)
+        {
+            cboSortAlgo.SelectedIndex = 0;
+        }
+
+        #region Variable Global
+        Button[] arrButton;
+        int[] arrValue;
+        int numArr;
+        Random rd = new Random();
+        int WIDTH = 25;  //kích thước khối
+        int GAP = 5;   //khoảng cách giữa các khối
+        int speed = 14; //max speed = 4ms, min speed = 14ms
+        int sleep = 420;  //thời gian dừng để quan sát các bước duyệt của thuật toán (sleep = 30 * speed)
+        bool sorting = false;
+        int sortSelected;   //Thuật toán sắp xếp muốn mô phỏng
+        #endregion
+
+        private void btnCreateArr_Click(object sender, EventArgs e)
+        {
+            if (sorting) return;
+            numArr = int.Parse(txtNumArr.Text);
+            arrValue = new int[numArr];
+            arrButton = new Button[numArr];
+
+            pnlSimulator.Controls.Clear();
+
+            for (int i = 0; i < numArr; i++)
+            {
+                int value = rd.Next(20, 150);
+                Button btn = new Button();
+                btn.FlatStyle = FlatStyle.Flat;
+                btn.FlatAppearance.BorderColor=Color.LightSkyBlue;
+                btn.Size = new Size(WIDTH, value);
+                //btn.Text = value.ToString();
+                btn.Location = new Point(
+                    (pnlSimulator.Width - (WIDTH + GAP) * numArr)/2 + (WIDTH + GAP) * i,
+                    pnlSimulator.Height - WIDTH - value);
+                btn.BackColor = Color.LightBlue;
+
+                pnlSimulator.Controls.Add(btn);
+
+                arrButton[i] = btn;
+                arrValue[i] = value;
+            }
+            btnSort.Enabled = true;
+            //MergeSort(0, numArr - 1);
+            //for (int i = 0; i < numArr; i++)
+            //{
+            //    txtNumArr.Text += arrValue[i] + " ";
+            //}
+        }
+
+        private void btnSort_Click(object sender, EventArgs e)
+        {
+            sorting = true;
+            btnSort.Enabled = false;
+            sortSelected = cboSortAlgo.SelectedIndex;
+            bgwSimuSort.RunWorkerAsync();          
+        }
+
+        //Change speed sort in runtime
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            speed = 14 - trackBar1.Value;
+            sleep = 30 * speed;
+        }
+
+        //swap p1 p2 of array arrValue
+        private void swap(int p1, int p2)
+        {
+            int Tmp = arrValue[p1];
+            arrValue[p1] = arrValue[p2];
+            arrValue[p2] = Tmp;
+        }
+
+        #region Simulation Functions
+        //Highlight color of button
+        private void HighLight(int pos, Color color)
+        {
+            if (pos < numArr)
+                arrButton[pos].BackColor = color;
+        }
+
+        private void MoveButton(int p1, int p2)
+        {
+            Status st = new Status();
+            st.Pos1 = p1;
+            st.Pos2 = p2;
+            int x;
+
+            //st.mType = MoveType.LINE_TO_TOP_AND_LINE_TO_BOTTOM;
+            //for (x = 0; x < SIZE; x++)
+            //{
+            //    bgwSimuSort.ReportProgress(0, st);
+            //    Thread.Sleep(10);
+            //}
+
+            st.mType = MoveType.LEFT_TO_RIGHT_AND_RIGHT_TO_LEFT;
+            int nLoop = ((WIDTH + GAP) * Math.Abs(st.Pos1 - st.Pos2)) / 2;
+            for (x = 0; x < nLoop; x++)
+            {
+                bgwSimuSort.ReportProgress(0, st);
+                Thread.Sleep(speed);
+            }
+
+            //st.mType = MoveType.TOP_TO_LINE_AND_BOTTOM_TO_LINE;
+            //for (x = 0; x < SIZE; x++)
+            //{
+            //    bgwSimuSort.ReportProgress(0, st);
+            //    Thread.Sleep(10);
+            //}
+
+            st.mType = MoveType.CHANGED;
+            bgwSimuSort.ReportProgress(0, st);
+            Thread.Sleep(speed);
+        }
+
+        private void bgwSimuSort_DoWork(object sender, DoWorkEventArgs e)
+        {
+            switch (sortSelected)
+            {
+                case 0:
+                    InterchangeSort();
+                    break;
+                case 1:
+                    InsertionSort();
+                    break;
+                case 2:
+                    SelectionSort();
+                    break;
+                case 3:
+                    BubbleSort();
+                    break;
+                case 4:
+                    HeapSort();
+                    break;
+                case 5:
+                    QuickSort(0, numArr - 1);
+                    break;
+                case 6:
+                    MergeSort(0, numArr - 1);
+                    break;
+                case 7:
+                    ShellSort();
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void bgwSimuSort_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            Status st = e.UserState as Status;
+            if (st == null) return;
+            if (st.mType == MoveType.CHANGED)
+            {
+                Button btnTmp = arrButton[st.Pos1];
+                arrButton[st.Pos1] = arrButton[st.Pos2];
+                arrButton[st.Pos2] = btnTmp;
+                return;
+            }
+
+            Button btn1 = arrButton[st.Pos1];
+            Button btn2 = arrButton[st.Pos2];
+            //if (st.mType == MoveType.LINE_TO_TOP_AND_LINE_TO_BOTTOM)
+            //{
+            //    btn1.Top = btn1.Top + 1;
+            //    btn2.Top = btn2.Top - 1;
+            //}
+            //else 
+            if (st.mType == MoveType.LEFT_TO_RIGHT_AND_RIGHT_TO_LEFT)
+            {
+                btn1.Left = btn1.Left - 2;
+                btn2.Left = btn2.Left + 2;
+            }
+            //else if (st.mType == MoveType.TOP_TO_LINE_AND_BOTTOM_TO_LINE)
+            //{
+            //    btn1.Top = btn1.Top - 1;
+            //    btn2.Top = btn2.Top + 1;
+            //}
+        }
+
+        private void bgwSimuSort_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            for (int i = 0; i < numArr; i++)
+            {
+                arrButton[i].BackColor = Color.LightBlue;
+            }
+            sorting = false;
+            MessageBox.Show("Đã sắp xếp xong");
+        }
+        #endregion
+
+        #region Sort Algorithms
+        private void BubbleSort()
+        {
+            bool swapped = true;
+
+            for (int i = 0; i < numArr - 1 && swapped; i++)
+            {
+                swapped = false;
+                for (int j = numArr - 1; j > i; j--)
+                {
+                    HighLight(j, Color.Green);
+                    HighLight(j - 1, Color.Green);
+                    Thread.Sleep(sleep);
+
+                    if (arrValue[j] < arrValue[j - 1])
+                    {
+                        HighLight(j, Color.Orange);
+                        HighLight(j - 1, Color.Orange);
+
+                        MoveButton(j, j - 1);
+                        swap(j, j - 1);
+                        swapped = true;
+
+                    }
+
+                    HighLight(j, Color.LightBlue);
+                    HighLight(j - 1, Color.LightBlue);
+                }
+                HighLight(i, Color.Orange);
+            }
+        }
+
+        private void InsertionSort()
+        {
+            for (int i = 1; i < numArr; i++)
+            {
+                int x = arrValue[i];
+                int j = i - 1;
+
+                HighLight(j, Color.Orange);
+                HighLight(i, Color.Red);
+                Thread.Sleep(sleep);
+
+                while (j >= 0 && arrValue[j] > x)
+                {
+                    HighLight(j, Color.Green);
+
+                    Thread.Sleep(sleep);
+                    arrValue[j  + 1] = arrValue[j];
+                    MoveButton(j + 1, j);
+                    //sau khi move, index của 2 button đã thay đổi
+                    HighLight(j + 1, Color.Orange);
+                    Thread.Sleep(sleep);
+                    j--;
+                }
+                HighLight(i, Color.Orange);
+                HighLight(j+1, Color.Orange);
+                Thread.Sleep(sleep);
+
+                arrValue[j + 1] = x;
+            }          
+        }
+
+        private void SelectionSort()
+        {
+            for(int i =0; i< numArr - 1; i++)
+            {
+                int minPos = i;
+                //change color to red
+                HighLight(minPos, Color.Red);
+                Thread.Sleep(sleep);
+                for (int j = i + 1; j < numArr; j++)
+                {
+                    //change color green
+                    HighLight(j, Color.Green);
+                    Thread.Sleep(sleep);
+                    if (arrValue[j] < arrValue[minPos])
+                    {
+                        //change color min to lightblue
+                        HighLight(minPos, Color.LightBlue);
+                        //change color j to red
+                        HighLight(j, Color.Red);
+                        minPos = j;
+                    }
+                    else HighLight(j, Color.LightBlue);
+                    Thread.Sleep(sleep);
+                    //else return color lightblue
+                }
+                
+                MoveButton(minPos, i);
+                swap(minPos, i);
+                //change color to Orange
+                HighLight(i, Color.Orange);
+            }
+        }
+
+        private void InterchangeSort()
+        {
+            for(int i = 0; i < numArr - 1; i++)
+            {
+                HighLight(i, Color.Red);
+                Thread.Sleep(sleep);
+
+                for(int j = i + 1; j<numArr; j++)
+                {
+                    HighLight(j, Color.Green);
+                    Thread.Sleep(sleep);
+
+                    if (arrValue[i] > arrValue[j])
+                    {
+                        HighLight(i, Color.LightBlue);
+                        HighLight(j, Color.Red);
+                        Thread.Sleep(sleep);
+
+                        MoveButton(j,i);
+                        swap(i, j);
+                    }
+                    HighLight(j, Color.LightBlue);
+                    Thread.Sleep(sleep);
+                }
+                HighLight(i, Color.Orange);
+            }
+        }
+
+        //Quick Sort with first element as pivot
+        private void QuickSort(int low, int high)
+        {
+            if (low < high)
+            {
+                int m = Partition(low, high); // O(N)
+                                                 // a[low..high] ~> a[low..m–1], pivot, a[m+1..high]
+                QuickSort(low, m - 1); // recursively sort left subarray
+                                          // a[m] = pivot is already sorted after partition
+                QuickSort(m + 1, high); // then sort right subarray
+            }
+            if (low < numArr)
+            {
+                HighLight(low, Color.Orange);
+                Thread.Sleep(sleep);
+            }
+        }
+        //Parition of Quick Sort Algorithms
+        private int Partition(int i, int j)
+        {
+            int p = arrValue[i]; // p is the pivot
+            HighLight(i, Color.Yellow);
+            int m = i; 
+            for (int k = i + 1; k <= j; k++)
+            { 
+                HighLight(k, Color.Red);
+                Thread.Sleep(sleep);
+                if (arrValue[k] < p)
+                { 
+                    m++;
+                    MoveButton(k, m);
+                    HighLight(m, Color.LightGreen);
+                    Thread.Sleep(sleep);
+                    swap(k, m); 
+                } 
+                else
+                {
+                    HighLight(k, Color.Purple);
+                    Thread.Sleep(sleep);
+                }
+            }
+            MoveButton(m, i);
+            swap(i, m); // final step, swap pivot with a[m]
+            for (int x = i; x <= j; x++)
+            {
+                HighLight(x, Color.LightBlue);
+            }
+
+            HighLight(m, Color.Orange);
+            Thread.Sleep(sleep);
+            return m; // return the index of pivot
+        }
+
+        void Merge(int low, int mid, int high)
+        {
+            // subarray1 = a[low..mid], subarray2 = a[mid+1..high], both sorted
+            int N = high - low + 1;
+            int[] b = new int[N]; // discuss: why do we need a temporary array b?
+            int left = low;
+            int right = mid + 1;
+            int bIdx = 0;
+            while (left <= mid && right <= high) // the merging
+            {
+                //HighLight(left, Color.Red);
+                //HighLight(right, Color.Red);
+                //Thread.Sleep(sleep);
+                if(arrValue[left] <= arrValue[right])
+                {
+                    b[bIdx++] = arrValue[left++];
+                }
+                else
+                {
+                    //MoveButton(right, left);
+                    b[bIdx++] = arrValue[right++];
+                }
+                //HighLight(left, Color.LightBlue);
+                //HighLight(right, Color.LightBlue);
+                //Thread.Sleep(sleep);
+                //b[bIdx++] = (arrValue[left] <= arrValue[right]) ? arrValue[left++] : arrValue[right++];
+            } 
+            while (left <= mid)
+                b[bIdx++] = arrValue[left++]; // leftover, if any
+            while (right <= high)
+                b[bIdx++] = arrValue[right++]; // leftover, if any
+            for (int k = 0; k < N; k++)
+                arrValue[low + k] = b[k]; // copy back
+        }
+
+        void MergeSort(int low, int high)
+        {
+            // the array to be sorted is a[low..high]
+            if (low < high)
+            { // base case: low >= high (0 or 1 item)
+                int mid = (low + high) / 2;
+                MergeSort(low, mid); // divide into two halves
+                MergeSort(mid + 1, high); // then recursively sort them
+                Merge(low, mid, high); // conquer: the merge subroutine
+            }
+        }
+
+        private void HeapSort()
+        {
+            //Build-Max-Heap
+            int heapSize = numArr;
+            for (int p = (heapSize - 1) / 2; p >= 0; p--)
+                MaxHeapify(heapSize, p);
+            for (int i =  heapSize- 1; i > 0; i--)
+            { 
+                MoveButton(i,0);
+                swap(i, 0);
+                heapSize--;
+                MaxHeapify(heapSize, 0);
+            }
+        }
+
+        private void MaxHeapify(int heapSize, int index)
+        {
+            int left = (index + 1) * 2 - 1;
+            int right = (index + 1) * 2;
+            int largest = 0;
+
+            if (left < heapSize && arrValue[left] > arrValue[index])
+                largest = left;
+            else
+                largest = index;
+            if (right < heapSize && arrValue[right] > arrValue[largest])
+            {
+                largest = right;
+            }
+            if (largest != index)
+            {
+                MoveButton(largest,index);
+                swap(index, largest);
+                MaxHeapify(heapSize, largest);
+            }
+        }
+
+
+        private void ShellSort()
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
